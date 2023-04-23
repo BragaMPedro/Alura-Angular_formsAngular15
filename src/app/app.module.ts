dfff5079dc5a5cdebf06ed6648560c61c90a1930
BragaMPedro <pedrobmagalhaes95@gmail.com>
2023-04-23 10:42:19 -0300

    Routes e Global imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SucessoCadastroComponent } from './components/sucesso-cadastro/sucesso-cadastro.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SucessoCadastroComponent,
+    CadastroComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
